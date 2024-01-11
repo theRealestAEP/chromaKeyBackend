0e4c8742f320dd76ccaeb719c8bff3bdceda1a24
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 16:42:53 -0500

    updated

--- index.ts
+++ index.ts
@@ -11,9 +11,11 @@ const chromaKeyVideo = (inputPath: string, outputPath: string, color: string, si
     return new Promise<void>((resolve, reject) => {
         ffmpeg()
             .input(inputPath)
-            .videoFilter(`colorkey=color=${color}:similarity=${similarity}:blend=${blend},scale='min(iw,ih)':min(iw,ih),pad=iw:ih:(ow-iw)/2:(oh-ih)/2:color=black@0.0`)
+            .complexFilter([
+                // Apply chroma keying, scale while maintaining aspect ratio, pad, and then scale to 800x800 - all in one filter string         
+                `colorkey=color=${color}:similarity=${similarity}:blend=${blend},scale=800:800:force_original_aspect_ratio=decrease,pad=800:800:(ow-iw)/2:(oh-ih)/2:color=black@0`
+            ])
             // Add a filter to scale the video while keeping its aspect ratio and add transparent padding to achieve a 1:1 ratio
-            // .videoFilter(`scale='min(iw,ih)':min(iw,ih),pad=iw:ih:(ow-iw)/2:(oh-ih)/2:color=black@0.0`)
             .outputOptions('-c:v', 'libvpx')
             .outputOptions('-auto-alt-ref', '0')
             .outputOptions('-pix_fmt', 'yuva420p')
